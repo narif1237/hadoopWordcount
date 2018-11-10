c7b6b7456b6befcc0fd41e2c934c26cf3e1aabb1
narif1237 <narif1237gmail.com>
2018-11-10 14:17:34 -0800

    Haddop wordcount. tracking changes to a file

---
+++ src/StubReducer.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class StubReducer extends Reducer<Text, IntWritable, Text, DoubleWritable> {
+
+  @Override
+  public void reduce(Text key, Iterable<IntWritable> values, Context context)
+      throws IOException, InterruptedException {
+
+    /*
+     * TODO implement
+     */
+	   int sum = 0;
+       for(IntWritable value:values)
+       {
+    	   sum=value.get()+sum;
+    	   
+       }
+       context.write(key,new DoubleWritable(sum));
+  }
+}
